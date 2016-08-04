4110660b1c6951829f88099c0de7bce1d42db5d2
Eric Lin <ericlin@ericlin.me>
2016-08-04 16:58:49 +1000

    updated pom.xml to add Cloudera Impala JDBC dependency and script to install JDBC driver

--- setup-jdbc.sh
+++ setup-jdbc.sh
@@ -2,8 +2,7 @@ version=2.5.4.1006
 target=Cloudera_HiveJDBC_${version}.zip
 wget https://downloads.cloudera.com/connectors/$target
 unzip $target
-mkdir Cloudera_HiveJDBC4_${version};
-cd Cloudera_HiveJDBC4_${version}
+mkdir jars 
+cd jars
 unzip ../Cloudera_HiveJDBC4_${version}.zip
-export CLASSPATH=$CLASSPATH:`pwd`/Cloudera_HiveJDBC4_${version}/*:`hadoop classpath`:/opt/cloudera/parcels/CDH-*/lib/hive/lib
 
